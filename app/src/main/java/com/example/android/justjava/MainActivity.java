97c555b82eb0d2e25d4595f511f5e6a35da50ed4
kcwang3 <sxxs123sxxs123@gmail.com>
2016-07-20 08:32:18 +0800

    version1

---
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.android.justjava;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends ActionBarActivity {
+
+    int quantity = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the plus button is clicked.
+     */
+    public void increment(View view) {
+        quantity=quantity+1;
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the minus button is clicked.
+     */
+    public void decrement(View view) {
+        quantity=quantity-1;
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        int price = calculatePrice();
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+        String priceMessage=createOrderSummary(price,hasWhippedCream);
+        displayMessage(priceMessage);
+        //displayPrice(quantity*5);
+    }
+
+    /**
+     * Calculates the price of the order.
+     * @return total price
+     */
+    private int calculatePrice(){
+        return quantity*5;
+    }
+
+    /**
+     * Create summary of the order.
+     * @param price of the order
+     * @param addWhippedCream is whether or not the user wants whipped cream topping
+     * @return text summary
+     */
+    private String createOrderSummary(int price,boolean addWhippedCream){
+        String priceMessage = "Name: Kun-Chi";
+        priceMessage = priceMessage + "\nAdd whipped cream?" + addWhippedCream;
+        priceMessage = priceMessage + "\nQuantity: " + quantity;
+        priceMessage = priceMessage + "\nTotal: $" + price;
+        priceMessage = priceMessage + "\nThank you.";
+        return priceMessage;
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    /*We don't need it.
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+    */
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
+    }
+}
